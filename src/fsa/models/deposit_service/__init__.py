5640a4065bc9c613405c16472103ea792a7d546d
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2022-05-31 23:43:38 -0600

    scaffolding

---
+++ src/fsa/models/deposit_service/__init__.py
@@ -0,0 +1,2 @@
+from fsa.models.deposit_service.checking_account import CheckingAccount
+from fsa.models.deposit_service.savings_account import SavingsAccount
---
+++ src/fsa/models/lending_service/__init__.py
@@ -0,0 +1,2 @@
+from fsa.models.lending_service.credit_card import CreditCard
+from fsa.models.lending_service.term_loan import TermLoan
